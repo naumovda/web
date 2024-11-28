d05455dd19aae7050710ca60caa526629780a968
naumov d a <naumov_rgrtu@mail.ru>
2024-11-28 11:27:00 +0300

    Added ex

---
+++ examples/WebApplication3/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationContext _context;
+
+        public List<User> Users { get; private set; } = new();
+
+        public IndexModel(ILogger<IndexModel> logger,
+            ApplicationContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            Users = _context.Users.AsNoTracking().ToList();
+        }
+    }
+}
---
+++ projects/journal/StudyJournal/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudyJournal.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Discipline> Disciplines { get; set; } = null!;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
